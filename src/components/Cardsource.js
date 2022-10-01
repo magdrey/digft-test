cc8af46d41ec41036d9e0715ac15845d29898155
magdrey <dreyjay24@gmail.com>
2022-10-02 00:39:40 +0100

    title commit

--- src/components/Cardsource.js
+++ src/components/Cardsource.js
@@ -1,167 +1,102 @@
 export const CardsContent1 = [
-    {
-      label: 'Giftcard',
-      title: 'Amazon Gift Card',
-      content:
-        'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-      info: 'This card has a fee of $ 0.99',
-      btn: 'Purchase card',
-    },
-    {
-      label: 'Giftcard',
-      title: 'Amazon Gift Card',
-      content:
-        'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-      info: 'This card has a fee of $ 0.99',
-      btn: 'Purchase card',
-    },
-    {
-      label: 'Giftcard',
-      title: 'Amazon Gift Card',
-      content:
-        'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-      info: 'This card has a fee of $ 0.99',
-      btn: 'Purchase card',
-    },
-    {
-      label: 'Giftcard',
-      title: 'Amazon Gift Card',
-      content:
-        'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-      info: 'This card has a fee of $ 0.99',
-      btn: 'Purchase card',
-    },
-    {
-        label: 'Giftcard',
-        title: 'Amazon Gift Card',
-        content:
-          'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-        info: 'This card has a fee of $ 0.99',
-        btn: 'Purchase card',
-      },
-      {
-        label: 'Giftcard',
-        title: 'Amazon Gift Card',
-        content:
-          'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-        info: 'This card has a fee of $ 0.99',
-        btn: 'Purchase card',
-      },
-      {
-        label: 'Giftcard',
-        title: 'Amazon Gift Card',
-        content:
-          'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-        info: 'This card has a fee of $ 0.99',
-        btn: 'Purchase card',
-      },
-      {
-        label: 'Giftcard',
-        title: 'Amazon Gift Card',
-        content:
-          'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-        info: 'This card has a fee of $ 0.99',
-        btn: 'Purchase card',
-      },
-      {
-        label: 'Giftcard',
-        title: 'Amazon Gift Card',
-        content:
-          'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-        info: 'This card has a fee of $ 0.99',
-        btn: 'Purchase card',
-      },
-      {
-        label: 'Giftcard',
-        title: 'Amazon Gift Card',
-        content:
-          'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-        info: 'This card has a fee of $ 0.99',
-        btn: 'Purchase card',
-      },
-      {
-        label: 'Giftcard',
-        title: 'Amazon Gift Card',
-        content:
-          'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-        info: 'This card has a fee of $ 0.99',
-        btn: 'Purchase card',
-      },
-      {
-        label: 'Giftcard',
-        title: 'Amazon Gift Card',
-        content:
-          'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-        info: 'This card has a fee of $ 0.99',
-        btn: 'Purchase card',
-      },
-      
-  ]
-  export const CardsContent2 = [
-    {
-      label: 'Giftcard',
-      title: 'Amazon Gift Card',
-      content:
-        'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-      info: 'This card has a fee of $ 0.99',
-      btn: 'Purchase card',
-    },
-    {
-      label: 'Giftcard',
-      title: 'Amazon Gift Card',
-      content:
-        'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-      info: 'This card has a fee of $ 0.99',
-      btn: 'Purchase card',
-    },
-    {
-      label: 'Giftcard',
-      title: 'Amazon Gift Card',
-      content:
-        'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-      info: 'This card has a fee of $ 0.99',
-      btn: 'Purchase card',
-    },
-    {
-      label: 'Giftcard',
-      title: 'Amazon Gift Card',
-      content:
-        'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-      info: 'This card has a fee of $ 0.99',
-      btn: 'Purchase card',
-    },
-  ]
-  export const CardContents3 = [
-    {
-      label: 'Giftcard',
-      title: 'Amazon Gift Card',
-      content:
-        'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-      info: 'This card has a fee of $ 0.99',
-      btn: 'Purchase card',
-    },
-    {
-      label: 'Giftcard',
-      title: 'Amazon Gift Card',
-      content:
-        'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-      info: 'This card has a fee of $ 0.99',
-      btn: 'Purchase card',
-    },
-    {
-      label: 'Giftcard',
-      title: 'Amazon Gift Card',
-      content:
-        'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-      info: 'This card has a fee of $ 0.99',
-      btn: 'Purchase card',
-    },
-    {
-      label: 'Giftcard',
-      title: 'Amazon Gift Card',
-      content:
-        'Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....',
-      info: 'This card has a fee of $ 0.99',
-      btn: 'Purchase card',
-    },
-  ]
+  {
+    label: "Giftcard",
+    title: "Amazon Gift Card",
+    content:
+      "Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....",
+    info: "This card has a fee of $ 0.99",
+    btn: "Purchase card",
+  },
+  {
+    label: "Giftcard",
+    title: "Amazon Gift Card",
+    content:
+      "Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....",
+    info: "This card has a fee of $ 0.99",
+    btn: "Purchase card",
+  },
+  {
+    label: "Giftcard",
+    title: "Amazon Gift Card",
+    content:
+      "Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....",
+    info: "This card has a fee of $ 0.99",
+    btn: "Purchase card",
+  },
+  {
+    label: "Giftcard",
+    title: "Amazon Gift Card",
+    content:
+      "Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....",
+    info: "This card has a fee of $ 0.99",
+    btn: "Purchase card",
+  },
+];
+export const CardsContent2 = [
+  {
+    label: "Giftcard",
+    title: "Amazon Gift Card",
+    content:
+      "Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....",
+    info: "This card has a fee of $ 0.99",
+    btn: "Purchase card",
+  },
+  {
+    label: "Giftcard",
+    title: "Amazon Gift Card",
+    content:
+      "Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....",
+    info: "This card has a fee of $ 0.99",
+    btn: "Purchase card",
+  },
+  {
+    label: "Giftcard",
+    title: "Amazon Gift Card",
+    content:
+      "Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....",
+    info: "This card has a fee of $ 0.99",
+    btn: "Purchase card",
+  },
+  {
+    label: "Giftcard",
+    title: "Amazon Gift Card",
+    content:
+      "Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....",
+    info: "This card has a fee of $ 0.99",
+    btn: "Purchase card",
+  },
+];
+export const CardContents3 = [
+  {
+    label: "Giftcard",
+    title: "Amazon Gift Card",
+    content:
+      "Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....",
+    info: "This card has a fee of $ 0.99",
+    btn: "Purchase card",
+  },
+  {
+    label: "Giftcard",
+    title: "Amazon Gift Card",
+    content:
+      "Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....",
+    info: "This card has a fee of $ 0.99",
+    btn: "Purchase card",
+  },
+  {
+    label: "Giftcard",
+    title: "Amazon Gift Card",
+    content:
+      "Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....",
+    info: "This card has a fee of $ 0.99",
+    btn: "Purchase card",
+  },
+  {
+    label: "Giftcard",
+    title: "Amazon Gift Card",
+    content:
+      "Amazon Gift Card is a global fashion destination for 20-somethings, selling all the freshest styles complemented by exclusive content....",
+    info: "This card has a fee of $ 0.99",
+    btn: "Purchase card",
+  },
+];
--- src/components/Homecards.jsx
+++ src/components/Homecards.jsx
@@ -1,115 +1,170 @@
-import React, { useState } from 'react';
-import { CardsContent1 } from './Cardsource';
+import React, { useState } from "react";
+import { CardContents3, CardsContent1, CardsContent2 } from "./Cardsource";
 import amacard from "../img/DarkAmazon.svg";
-import info from "../img/info.svg"
+import info from "../img/info.svg";
 
 const Homecards = () => {
-
-
-
-
-
-
-
-    const ShowCards = () => {
-
-        return (
-            <>
-                {CardsContent1.map((CardsContent, index) => {
-                    return (
-                        <>
-
-                            <div className="card-cont">
-                                <div className=" cardcard" key={index}>
-                                    <h5 className="card-label">{CardsContent.label}</h5>
-                                    <div className="card-title-pic">
-                                        <img src={amacard} className="card-img-top" height="25px" />
-                                        <h5 className="card-title">{CardsContent.title}</h5>
-                                    </div>
-                                    <div className="card-body">
-                                        <h5 className="card-content">{CardsContent.content}</h5>
-                                        <p className="card-info"> <img src={info} className="info-icon" />  {CardsContent.info}</p>
-
-                                        <button className="card-btn" > {CardsContent.btn} </button>
-
-                                    </div>
-
-                                </div>
-                            </div>
-                        </>
-                    )
-                })}
-
-            </>
-        )
-    };
-
-
-
-
-
-    const latestfilters =
-        ['Sports & Entertainment', 'Home and furnitures', " Jewelry & Watches", 'Clothes and Fashion', 'Electronics', 'Gaming', 'Airtime & Electricity', 'Popular', 'Ecommerce']
-    const [checked, setChecked] = useState([]);
-    const check = (event) => {
-        var updatedList = [...checked]
-        if (event.target.checked) {
-            updatedList = [...checked, event.target.value]
-        } else {
-            updatedList.splice(checked.indexOf(event.target.value), 1)
-        }
-        setChecked(updatedList)
-
-    };
-
-    var isChecked = (item) =>
-        checked.includes(item) ? 'checked-item' : 'not-checked-item';
-
-
-
-
-
-
-
-
+  const ShowCards = () => {
     return (
-        <div>
-            <h2 className="latearr"> Latest Arrivals </h2>
-            <div className="homecardbody">
-                <div className='sidefilter'>
-                    <div className="fillt">
-                        <p>Filter</p> <p>Reset</p>
+      <>
+        <div className="card-group">
+          {CardsContent1.map((CardsContent, index) => {
+            return (
+              <>
+                <div className="card-cont">
+                  <div className=" cardcard" key={index}>
+                    <h5 className="card-label">{CardsContent.label}</h5>
+                    <div className="card-title-pic">
+                      <img
+                        src={amacard}
+                        alt="img"
+                        className="card-img-top"
+                        height="25px"
+                      />
+                      <h5 className="card-title">{CardsContent.title}</h5>
                     </div>
-                    <div className="filterlist">
-                        <p className="flh">Categories</p>
+                    <div className="card-body">
+                      <h5 className="card-content">{CardsContent.content}</h5>
+                      <p className="card-info">
+                        {" "}
+                        <img src={info} alt="img" className="info-icon" />{" "}
+                        {CardsContent.info}
+                      </p>
 
-                        {latestfilters.map((filt) => (
-                            <div key={filt} className="form-check">
-                                <input className="form-check-input" type="checkbox" value={filt} onChange={check} id="filtitem" />
-                                <label id="filtitem" className={isChecked(filt)}>
-                                    {filt}
-                                </label></div>
-                        ))
-                        }
-
-
-
-
-
-
-
+                      <button className="card-btn"> {CardsContent.btn} </button>
                     </div>
-
+                  </div>
                 </div>
-                <div className="cardss">
-                    {<ShowCards />}
-
-                </div>
-
-
-            </div>
+              </>
+            );
+          })}
         </div>
-    )
-}
 
-export default Homecards
+        <div className="card-group">
+          {CardsContent2.map((CardsContent, index) => {
+            return (
+              <>
+                <div className="card-cont">
+                  <div className=" cardcard" key={index}>
+                    <h5 className="card-label">{CardsContent.label}</h5>
+                    <div className="card-title-pic">
+                      <img
+                        src={amacard}
+                        alt="img"
+                        className="card-img-top"
+                        height="25px"
+                      />
+                      <h5 className="card-title">{CardsContent.title}</h5>
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-content">{CardsContent.content}</h5>
+                      <p className="card-info">
+                        {" "}
+                        <img src={info} alt="img" className="info-icon" />{" "}
+                        {CardsContent.info}
+                      </p>
+
+                      <button className="card-btn"> {CardsContent.btn} </button>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+
+        <div className="card-group">
+          {CardContents3.map((CardsContent, index) => {
+            return (
+              <>
+                <div className="card-cont">
+                  <div className=" cardcard" key={index}>
+                    <h5 className="card-label">{CardsContent.label}</h5>
+                    <div className="card-title-pic">
+                      <img
+                        src={amacard}
+                        alt="img"
+                        className="card-img-top"
+                        height="25px"
+                      />
+                      <h5 className="card-title">{CardsContent.title}</h5>
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-content">{CardsContent.content}</h5>
+                      <p className="card-info">
+                        {" "}
+                        <img src={info} alt="img" className="info-icon" />{" "}
+                        {CardsContent.info}
+                      </p>
+
+                      <button className="card-btn"> {CardsContent.btn} </button>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </>
+    );
+  };
+
+  const latestfilters = [
+    "Sports & Entertainment",
+    "Home and furnitures",
+    " Jewelry & Watches",
+    "Clothes and Fashion",
+    "Electronics",
+    "Gaming",
+    "Airtime & Electricity",
+    "Popular",
+    "Ecommerce",
+  ];
+  const [checked, setChecked] = useState([]);
+  const check = (event) => {
+    var updatedList = [...checked];
+    if (event.target.checked) {
+      updatedList = [...checked, event.target.value];
+    } else {
+      updatedList.splice(checked.indexOf(event.target.value), 1);
+    }
+    setChecked(updatedList);
+  };
+
+  var isChecked = (item) =>
+    checked.includes(item) ? "checked-item" : "not-checked-item";
+
+  return (
+    <div>
+      <h2 className="latearr"> Latest Arrivals </h2>
+      <div className="homecardbody">
+        <div className="sidefilter">
+          <div className="fillt">
+            <p>Filter</p> <p>Reset</p>
+          </div>
+          <div className="filterlist">
+            <p className="flh">Categories</p>
+
+            {latestfilters.map((filt) => (
+              <div key={filt} className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value={filt}
+                  onChange={check}
+                  id="filtitem"
+                />
+                <label id="filtitem" className={isChecked(filt)}>
+                  {filt}
+                </label>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="cardss">{<ShowCards />}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Homecards;
